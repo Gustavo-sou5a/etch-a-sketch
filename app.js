4a97ffa38c1e5fb6974ca97dd8c7683ec53724b3
Gustavo-sou5a <gustavo.gomesdesousa@gmail.com>
2025-02-20 16:18:26 +0000

    Add button to change the number of squares per side
    This button will prompt the user for an input for
    the number of squares per side, where the number
    cannot be larger than 100 nor negative (repropmpting
    the user in those cases)

--- app.js
+++ app.js
@@ -1,37 +1,54 @@
-const NUM_SQUARES_PER_SIDE = 16;
-const CONTAINER_SIDE = 16*4*10;
+const body = document.querySelector("body");
+const btn = document.createElement("button");
+btn.textContent = "Click me!";
+btn.style.margin = "25px";
+btn.addEventListener("click", promptNumSquares);
 
-const container = document.querySelector("div");
-container.style.width = `${CONTAINER_SIDE}px`;
-container.style.height = `${CONTAINER_SIDE}px`;
+let num = 16;
+main();
 
-for (let i = 0; i < NUM_SQUARES_PER_SIDE; i++) {
-    let line = document.createElement("div");
-    line.classList.add("line");
-    line.style.height = `${CONTAINER_SIDE / NUM_SQUARES_PER_SIDE}px`;
-    line.style.width = `${CONTAINER_SIDE}px`;
-    container.appendChild(line);
+function promptNumSquares() {
+    do {
+        num = parseInt(prompt("Number of squares per side:"));
+    } while (num > 100 || num <= 0);
+    resetDOM();
+    main();
+}
+
+function resetDOM() {
+    body.removeChild(document.querySelector("div"));
+    const container = document.createElement("div");
+    container.setAttribute("id", "container");
+    body.appendChild(container);
+}
+
+function main() {
+    const NUM_SQUARES_PER_SIDE = num;
+    const CONTAINER_SIDE = 16*4*10;
+
+    const container = document.querySelector("#container");
+    container.style.width = `${CONTAINER_SIDE}px`;
+    container.style.height = `${CONTAINER_SIDE}px`;
+
+    body.insertBefore(btn, container);
+
+    for (let i = 0; i < NUM_SQUARES_PER_SIDE; i++) {
+        let line = document.createElement("div");
+        line.classList.add("line");
+        line.style.height = `${CONTAINER_SIDE / NUM_SQUARES_PER_SIDE}px`;
+        line.style.width = `${CONTAINER_SIDE}px`;
+        container.appendChild(line);
     
-    for (let j = 0; j < NUM_SQUARES_PER_SIDE; j++){
-        let square = document.createElement("div");
-        square.classList.add("square");
-        square.style.height = `${CONTAINER_SIDE / NUM_SQUARES_PER_SIDE}px`;
-        square.style.width = `${CONTAINER_SIDE / NUM_SQUARES_PER_SIDE}px`;
-        line.appendChild(square);
+        for (let j = 0; j < NUM_SQUARES_PER_SIDE; j++){
+            let square = document.createElement("div");
+            square.classList.add("square");
+            square.style.height = `${CONTAINER_SIDE / NUM_SQUARES_PER_SIDE}px`;
+            square.style.width = `${CONTAINER_SIDE / NUM_SQUARES_PER_SIDE}px`;
+            line.appendChild(square);
 
-        // square.addEventListener("mouseover", () =>  { square.style.backgroundColor = "red"; });
-        // square.addEventListener("mouseleave", () =>  { square.style.backgroundColor = ""; });
-
-        square.addEventListener("mouseover", (e) => mouseOverEvent(e));
-        square.addEventListener("mouseleave", (e) => mouseLeaveEvent(e));
+            square.addEventListener("mouseover", () => {square.style.backgroundColor = "red";});
+            square.addEventListener("mouseleave", () => {square.style.backgroundColor = "green";});
+        }
     }
-}
-
-function mouseOverEvent(e) {
-    e.target.style.backgroundColor = "red";
-}
-
-function mouseLeaveEvent(e) {
-    e.target.style.backgroundColor = "green";
 }
 
